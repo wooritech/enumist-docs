c7f2572de564346336afc9c660a5c17bca01ea07
Seo Seunghyun <tea7day@gmail.com>
2019-03-18 16:11:18 +0900

    문서 구조 생성 및 도메인 변경 (guides.enumist.com)

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,18 +1,37 @@
 module.exports = {
-  title: 'enumist',
+  title: 'enumist 가이드',
   description: '임상시험 피험자 관리 서비스',
   base: '/',
   themeConfig: {
     sidebar: [
+      'basics/introduction',
+      'basics/getting-started',
       {
-        title: '가이드',
-        collapsable: false,
-        children: ['/guide/instruction', '/guide/getting-started'],
+        title: '사용자 관리',
+        collapsable: true,
       },
       {
-        title: '고급',
-        collapsable: false,
-        children: ['/advanced/subject-01'],
+        title: '임상시험 관리',
+        collapsable: true,
+      },
+      {
+        title: '피험자 관리',
+        collapsable: true,
+      },
+      {
+        title: '의뢰업체 관리',
+        collapsable: true,
+      },
+      {
+        title: '알림 관리',
+        collapsable: true,
+      },
+      {
+        title: '자원 관리',
+        collapsable: true,
+      },
+      {
+        title: '설정',
       },
     ],
   },
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# 에누미스트 문서
+# 에누미스트 가이드
 
 [![Build Status](https://travis-ci.com/wooritech/enumist-docs.svg?branch=master)](https://travis-ci.com/wooritech/enumist-docs)
